60ed2dcdc9a98c6201fa878425aabfcefde71631
Antoine VINCENT <anvincen@e2r6p13.clusters.42paris.fr>
2023-05-16 18:09:11 +0200

    Working on the signal and the closure of the shell

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anvincen <anvincen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:30:55 by anvincen          #+#    #+#             */
-/*   Updated: 2023/05/16 16:47:12 by anvincen         ###   ########.fr       */
+/*   Updated: 2023/05/16 18:01:45 by anvincen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	sig_handler(int signal, siginfo_t *info, void *a)
 
 void	read_line(t_data *data)
 {
-	data->arg = readline("");
+	data->arg = readline(">>>");
 	printf("arg == %s\n", data->arg);
 	free(data->arg);
 }
@@ -42,11 +42,14 @@ int	main(void)
 	struct sigaction	s_sig;
 	t_data				data;
 
+	data.finished = false;
 	s_sig.sa_sigaction = sig_handler;
 	s_sig.sa_flags = SA_SIGINFO;
 	sigemptyset(&s_sig.sa_mask);
 	sigaction(SIGINT, &s_sig, 0);
-	while (1)
-		pause();
+	while (data.finished == false)
+	{
+		read_line(&data);
+	}
 	return (0);
 }
