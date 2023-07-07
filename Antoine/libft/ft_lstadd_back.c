e69102e92e7a05b9729c8425ee3368ec6b41f539
Antoine VINCENT <anvincen@paul-f4Ar5s8.clusters.42paris.fr>
2023-07-07 16:08:55 +0200

    rm files from test

---
+++ Antoine/libft/ft_lstadd_back.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anvincen <anvincen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 07:49:05 by anvincen          #+#    #+#             */
+/*   Updated: 2022/11/16 10:24:39 by anvincen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*ptr;
+
+	if (!lst || !new)
+		return ;
+	ptr = ft_lstlast(*lst);
+	if (ptr)
+		ptr->next = new;
+	else
+		*lst = new;
+}
