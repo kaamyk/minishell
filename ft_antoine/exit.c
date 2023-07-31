a5082ac8254d5bc7a845f25f58c175121651a34b
Antoine VINCENT <anvincen@paul-f5Br5s7.clusters.42paris.fr>
2023-07-31 18:28:38 +0200

    Solved the error message in exit

--- ft_antoine/exit.c
+++ ft_antoine/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
+/*   By: anvincen <anvincen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 16:53:07 by xuluu             #+#    #+#             */
-/*   Updated: 2023/07/21 09:39:46 by antoine          ###   ########.fr       */
+/*   Updated: 2023/07/31 18:26:03 by anvincen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ bool	check_alnum(char *arg)
 	i = 0;
 	while (arg[i] != 0)
 	{
-		if (ft_isdigit(arg[i]) == 0)
+		if (ft_isdigit(arg[i]) == 0 && arg[i] != ' ')
 			return (1);
 		++i;
 	}
@@ -44,17 +44,19 @@ void	ft_exit(t_data *data)
 {
 	if (data->arg == NULL || ft_strlen(data->arg) == 0)
 		exit (0);
-	else if (check_nb_arg(data->arg) != 0)
-	{
-		printf("bash: exit: too many arguments\n");
-		return ;
-	}
 	else if (check_alnum(data->arg) != 0)
 	{
 		free_list(data->tab_cmd);
 		free_list(data->env);
-		printf("bash: exit: %s: numeric argument required\n", data->arg);
+		write(STDERR_FILENO, "bash: exit:", 11);
+		write(STDERR_FILENO, data->arg, ft_strlen(data->arg));
+		write(STDERR_FILENO, ": numeric argument required\n", 28);
 		exit (2);
+	}
+	else if (check_nb_arg(data->arg) != 0)
+	{
+		write(STDERR_FILENO, "bash: exit: too many arguments\n", 32);
+		return ;
 	}
 	free_list(data->tab_cmd);
 	exit (ft_atoi(data->arg));
